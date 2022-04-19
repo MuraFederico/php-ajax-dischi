e15edc9b0ec4a810e79ba84278686f74dc00df5e
MuraFederico <federicomura98@gmail.com>
2022-04-19 11:42:00 +0200

    added bonus

--- api.php
+++ api.php
@@ -1,5 +1,19 @@
 <?php 
     include __DIR__ . './db/data.php';
+
+    $genre = $_GET['genre'] ?? '';
+    
+
+    if ($genre) {
+        $arr_results = [];
+        foreach($arr_disc as $disc) {
+            if ($disc['genre'] === $genre) {
+                $arr_results[] = $disc;
+            }
+        };
+    }else {
+        $arr_results = $arr_disc;
+    }
+
     header('Content-Type: application/json');
-    echo json_encode($arr_disc);
-?>
+    echo json_encode($arr_results);
--- vue/script.js
+++ vue/script.js
@@ -2,9 +2,25 @@ var app = new Vue({
     el: '#app',
     data: {
       arrDiscs: [],
+      arrGenre: [''],
+    },
+    methods: {
+      getArrGenre() {
+        this.arrDiscs.forEach((disc) => {
+          if (!this.arrGenre.includes(disc.genre)) {
+            this.arrGenre.push(disc.genre);
+          }
+        });
+      },
+      search(event) {
+        axios.get(`http://localhost/php-ajax-dischi/api.php?genre=${event.target.value}`)
+        .then(res => {this.arrDiscs = res.data});
+      },
     },
     created() {
-        axios.get('http://localhost/php-ajax-dischi/api.php')
-            .then(res => this.arrDiscs = res.data)
+        axios.get('http://localhost/php-ajax-dischi/api.php?')
+        .then(res => {this.arrDiscs = res.data
+            this.getArrGenre();
+        });
     }
   })
